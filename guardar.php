4a2d0facfbd9729e7085e339ff3c2b93cd8aa4ed
DaNySLagar <104644297+DaNySLagar@users.noreply.github.com>
2023-01-04 21:22:18 -0500

    crear usuario

--- guardar.php
+++ guardar.php
@@ -22,13 +22,15 @@
     $sql = "INSERT INTO tblusers (USERID, FULLNAME, USERNAME, PASS, ROLE) VALUES ('', '$fullname', '$usuario', '$h_upass', '$rol')";
 
     if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
+?>
+        <script>
+            window.location.replace("login.php");
+        </script>
+<?php
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 
     mysqli_close($conn);
 ?>
-    <script>
-        window.location.replace("login.php");
-    </script>
+
